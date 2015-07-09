1b4d61acfe884f041a14f878082e90d53b1651c7
Art-Vanderlay <tomsarantis@gmail.com>
2015-07-09 14:22:40 +1000

    Update adbtool.sh

--- adbtool.sh
+++ adbtool.sh
@@ -14,20 +14,21 @@ echo "      ||                                                                  
 echo "      ||                         CHOOSE FROM THE MENU                     ||"
 echo "      ||                                                                  ||"
 echo "      ||                                                                  ||"
-echo "      ||   1) install adb & fastboot            8) reboot recovery        ||"
+echo "      ||   1) install adb & fastboot           9) reboot bootloader       ||"
 echo "      ||                                                                  ||"
-echo "      ||   2) adb pull                          9) reboot bootloader      ||"
+echo "      ||   2) adb pull                         10) install apk            ||"
 echo "      ||                                                                  ||"
-echo "      ||   3) adb push                          10) install apk           ||"
+echo "      ||   3) adb push                         11) unlock bootloader      ||"
 echo "      ||                                                                  ||"
-echo "      ||   4) adb sideload                      11) unlock bootloader     ||"
+echo "      ||   4) adb sideload                     12) take screenshot        ||"
 echo "      ||                                                                  ||"
-echo "      ||   5) adb shell                         12) take screenshot       ||"
+echo "      ||   5) adb shell                        13) factory reset          ||"
 echo "      ||                                                                  ||"
-echo "      ||   6) take logcat                       13) factory reset         ||"
+echo "      ||   6) take logcat                      14) quit                   ||"
 echo "      ||                                                                  ||"
-echo "      ||   7) reboot                            14) quit                  ||"
-echo "      ||                                                                  ||"
+echo "      ||   7) reboot                           15) configure udev rules   ||"
+echo "      ||                                           (for no permissions    ||"
+echo "      ||   8) reboot recovery                      error)                 ||"
 echo "      ||                                                                  ||"
 echo "       ===================================================================="   
 echo ""
@@ -168,4 +169,46 @@ fi
 
 if [ $first -eq 14 ]
     then exit
+fi
+
+if [ $first -eq 15 ]; then
+
+cat >/tmp/android.rules << "EOF"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="0bb4", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="0e79", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="0502", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="0b05", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="413c", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="0489", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="091e", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="18d1", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="0bb4", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="12d1", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="24e3", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="2116", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="0482", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="17ef", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="1004", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="22b8", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="0409", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="2080", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="0955", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="2257", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="10a9", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="1d4d", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="0471", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="04da", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="05c6", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="1f53", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="04e8", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="04dd", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="0fce", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="0930", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="19d2", MODE="0666"
+EOF
+
+fi
+
+if [ -f /tmp/android.rules ]; then
+sudo cp /tmp/android.rules /etc/udev/rules.d/51-android.rules;sudo chmod 644   /etc/udev/rules.d/51-android.rules;sudo chown root. /etc/udev/rules.d/51-android.rules;sudo service udev restart;sudo killall adb;
 fi
